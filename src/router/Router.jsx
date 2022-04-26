e8f992e53323ebdc85d0046143b6d85e48ecba35
eakarr <eaakar.55@gmail.com>
2022-04-27 00:43:16 +0300

    isLoggedIn check added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Account from "../pages/Account/Account";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
@@ -7,22 +9,31 @@ import SignUp from "../pages/SignUp/SignUp";
 import UploadProduct from "../pages/UploadProduct/UploadProduct";
 
 const Router = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("AccessToken")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <>
       <Routes>
-        <>
-          <Route path="/" element={<Home />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/upload-product" element={<UploadProduct />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/upload-product" element={<UploadProduct />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </>
         ) : (
-        <>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </>
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </>
+        )}
       </Routes>
     </>
   );
