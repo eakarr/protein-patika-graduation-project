e3919c052025e8560d7d152f5193eb9f7dd63fd7
eakarr <eaakar.55@gmail.com>
2022-05-02 01:29:09 +0300

    Access token added to headers part

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+
+const getToken = Cookies.get("AccessToken");
 
 export default axios.create({
   baseURL: `https://bootcamp.akbolat.net`,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+  headers: getToken
+    ? { Authorization: `Bearer ${getToken}` }
+    : { Accept: "application/json", "Content-Type": "application/json" },
 });
