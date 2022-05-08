f80045f6ffda7ea1ba039a9214f59009c1e233f8
eakarr <eaakar.55@gmail.com>
2022-05-08 23:20:05 +0300

    Initial commit

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import Header from "../../components/Header/Header";
+import Email from "../../components/Email/Email";
+import Offers from "../../components/Offers/Offers";
+import "./Account.scss";
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="account-container">
+        <Email />
+        <Offers />
+      </div>
+    </div>
+  );
+};
 
-export default Account
+export default Account;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import Categories from "../../components/Categories/Categories";
+import Products from "../../components/Products/Products";
+
+import bannerImage from "../../assets/bannerImage2x.png";
+import "./Home.scss";
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [categoryIds, setCategoryIds] = useState([]);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="home-main-container">
+        <div className="bottom-container">
+          <img src={bannerImage} alt="Banner" className="banner" />
+          <Categories setSelectedCategory={setSelectedCategory} setCategoryIds={setCategoryIds} />
+          <Products selectedCategory={selectedCategory} categoryIds={categoryIds} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
